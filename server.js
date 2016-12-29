1e5ce1471e425b8c656e01e09ff4fe8ea624cdf2
Gino <masterlewis04@yahoo.co.uk>
2016-12-29 18:00:33 +0000

    converts natural dates to unix timestamps

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require("express");
+var timestamp = require('unix-timestamp');
 var app = express();
 
-app.get('/', function(req, res){
-    res.end('hello world');
+app.get('/:date', function(req, res){
+    var date = req.params.date;
+    var unix = timestamp.fromDate(date);
+    
+    if(isNaN(unix)) {
+        unix = null;     
+        date = null;
+    }
+    
+    var jsonDate = {
+        'unix': unix,
+        'natural': date
+    }
+    res.end(JSON.stringify(jsonDate));
 });
 
 app.listen(8080);
